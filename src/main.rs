adc0ce3aa0404684b658e8c4eb39f4d693d52135
yanknvim <yanknvim@email.com>
2026-03-13 13:23:11 +0900

    Add panic

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,10 @@ pub fn kernel_main() -> ! {
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_panic: &PanicInfo<'_>) -> ! {
+fn panic(info: &PanicInfo<'_>) -> ! {
+    println!("====== KERNEL PANIC! ======");
+    println!("{}", info);
+
+    sbi_rt::system_reset(sbi_rt::Shutdown, sbi_rt::NoReason);
     loop {}
 }
