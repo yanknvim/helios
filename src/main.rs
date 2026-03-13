5d0291ad080310ed91b28345626c0053eb15e298
yanknvim <yanknvim@email.com>
2026-03-13 10:23:08 +0900

    Hello World

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+#![no_main]
+#![no_std]
+
+use core::arch::naked_asm;
+use sbi_rt::console_write_byte;
+
+#[unsafe(no_mangle)]
+#[unsafe(link_section = ".text.boot")]
+#[unsafe(naked)]
+pub extern "C" fn boot() -> ! {
+    naked_asm!(
+        "la sp, __stack_top\n
+        j {kernel_main}\n",
+        kernel_main = sym kernel_main,
+    );
+}
+
+#[unsafe(no_mangle)]
+pub fn kernel_main() -> ! {
+    for &c in b"Hello, World!\n" {
+        console_write_byte(c);
+    }
+
+    loop {}
+}
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    loop {}
+}
